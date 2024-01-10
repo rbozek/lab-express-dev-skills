3c2b9f1ed4e0d8b651632f588efb4d8be592f024
rbozek <rbozek@gmail.com>
2024-01-09 23:22:41 -0500

    add data folder and skills data

---
+++ data/skills-data.js
@@ -0,0 +1,11 @@
+const skills = [
+ {text: 'burp on command', impressive: true, loud: true, _id: 100001},
+ {text: 'speed reading', impressive: true, loud: false, _id: 100002},
+ {text: 'complaining', impressive: false, loud: true, _id: 100003},
+ {text: 'massive hot dog consumption', impressive: false, loud: false, _id: 100004},
+ {text: 'sean connery impression', impressive: false, loud: true, _id: 100005}
+]
+
+export { 
+skills
+}

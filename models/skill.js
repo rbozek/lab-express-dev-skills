66caef7de4fc6e549e686226ae2378e6a6feec56
rbozek <rbozek@gmail.com>
2024-01-10 22:02:17 -0500

    create models directory & js file. define basic schema for skill. compile schema into model.

---
+++ models/skill.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const skillSchema = new Schema({
+  text: String,
+  done: Boolean,
+})
+
+// Compile the schema into a model and export it
+const Skill = mongoose.model('Skill', skillSchema)
+
+export {
+  Skill
+}
